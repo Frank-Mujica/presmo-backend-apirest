838979eed6cf2930443bca9b2c28b8cd818b47cf
Frank Mujica <45410072+Frank-Mujica@users.noreply.github.com>
2019-06-08 21:12:39 -0400

    v1

---
+++ src/main/java/com/cesfam/presmo/backend/apirest/PresmoBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.cesfam.presmo.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresmoBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresmoBackendApirestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cesfam/presmo/backend/apirest/models/entity/Usuario.java
@@ -0,0 +1,96 @@
+package com.cesfam.presmo.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity 
+@Table(name="usuarios")
+public class Usuario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(length=30)
+	private String nombre;
+	@Column(length=50)
+	private String apellidos;
+	@Column(unique=true)
+	private String email;
+	@Column(unique=true)
+	private String password;
+	@Column(name="tipo_usuario")
+	private String tipoUsuario;
+	@Column(name="created_at")
+	@Temporal(TemporalType.DATE)
+	private Date createdAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(String tipo) {
+		this.tipoUsuario = tipo;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
