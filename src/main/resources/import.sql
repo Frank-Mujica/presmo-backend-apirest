3aa4269618cf105f11b86231f30d5c6a5203dfa6
Frank Mujica <45410072+Frank-Mujica@users.noreply.github.com>
2019-06-30 19:09:07 -0400

    add tables

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,8 +13,8 @@ INSERT INTO articulos (id_tipo, id_fabricante, descripcion, componentes, conteni
 INSERT INTO usuarios (username, nombre, apellidos, email, password, enabled) VALUES ('frank666', 'frank', 'mujica fuentealba', 'f.mujica1995@gmail.com', '$2a$10$1/FWMTqX/H4enA5s9k.ZGupTEudJhq0mk1QQMzYvRGjpdBw.nwQSu', 2);
 INSERT INTO usuarios (username, nombre, apellidos, email, password, enabled) VALUES ('suicideboy', 'suicide', 'boys', 's.boys666@gmail.com', '$2a$10$ua67IMKby8nIhNyS5LxXZezHbxz0b1zfjH13dp2gsNCnA6jxbPKg6', 1);
 
-INSERT INTO roles (id, nombre) VALUES (1, 'ROLE_MEDICO');
-INSERT INTO roles (id, nombre) VALUES (2, 'ROLE_FARMACEUTICO');
+INSERT INTO roles (id, nombre) VALUES ('Medico');
+INSERT INTO roles (id, nombre) VALUES ('Farmaceutico');
 
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 2);
