ce50562f2875b5466ef90ed0fa58d4fa8ed4b470
Frank Mujica <45410072+Frank-Mujica@users.noreply.github.com>
2019-07-07 07:45:04 -0400

    classes & controllers fixed

--- src/main/java/com/cesfam/presmo/backend/apirest/controllers/PartidaRestController.java
+++ src/main/java/com/cesfam/presmo/backend/apirest/controllers/PartidaRestController.java
@@ -7,8 +7,6 @@ import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
--- src/main/java/com/cesfam/presmo/backend/apirest/models/entity/Partida.java
+++ src/main/java/com/cesfam/presmo/backend/apirest/models/entity/Partida.java
@@ -3,12 +3,14 @@ package com.cesfam.presmo.backend.apirest.models.entity;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
@@ -16,6 +18,8 @@ import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@Entity
+@Table(name = "partidas")
 public class Partida {
 
 	/**
